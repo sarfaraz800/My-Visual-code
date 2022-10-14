da5996de02ea21ae73e3859642f38e49682619bd
sarfaraz800 <sarfarazalex841405@gmail.com>
2022-10-14 21:38:02 +0530

    Updated

--- python codes/gcd.py
+++ python codes/gcd.py
@@ -1,19 +1,12 @@
 def gcd(m,n):
-    fm=[]
-    for i in range(1,m+1):
-        if(m%i==0):
-            fm.append(i)
-    fn=[]
-    for j in range(1,n+1):
-        if(n%j==0):
-            fn.append(j)
-    cf=[]
-    for f in fm:
-        if f in fn:
-            cf.append(f)
-    return (cf[-1])    
-print("Enter two number")
-m=int(input())
+    if m<n:
+        (m,n)=(n.m)
+    if m%n==0:
+        return (n)
+    else:
+        return (gcd(n,m%n))          
+
+m=int(input("Enter two number"))
 n=int(input())
 k=gcd(m,n)
 print("GCD is ",k)                
